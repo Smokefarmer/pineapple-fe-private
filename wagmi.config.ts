f4d64a12fbbac16a1598d317f1bccd98ccc865cc
Thomas Rauchbauer <rauchbauer.t@gmail.com>
2025-08-24 11:47:48 +0100

    Update BSC Testnet addresses in wagmi.config.ts. Changed contract addresses for systemContext, registry, factory, router, verifyTypedData, masterTaxHandler, and masterLaunchWhitelist to reflect the latest deployment, ensuring accurate configuration for the test environment.

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -23,13 +23,13 @@ const addresses =  {
 
 const addressesTest = {
   "network": "bscTestnet",
-  "systemContext": "0xf9c0ec13bbd861d54c4453b8f2d34507ae9b5f3d",
-  "registry": "0x7cac71adaf1399b6b0a6321d5c571bf9fa203dc0",
-  "factory": "0x7cf2f664c77420e00fb4abd954d1f7cfc81eb8dc",
-  "router": "0x934bc099c6589a2b7058edfbaf610e7dd767faaa",
-  "verifyTypedData": "0x82a45d7e36deb48f4314bfa38882cb85905f986f",
-  "masterTaxHandler": "0x6cffb2372d66c039d129b7ef5f25fb86bc9de31a",
-  "masterLaunchWhitelist": "0x1c2d024794986c8c5e5e94e528b500f54433c4f3",
+  "systemContext": "0x255843ce3cb2cd4c9ec82306da897ae434156866",
+  "registry": "0xe7a54db71dcf6dade82c6d4b525f23435dff506a",
+  "factory": "0xd7112286e89990c6af77994f836ec7761ac155ba",
+  "router": "0xd82a1760fdc7218263fa761958e1c40675c26728",
+  "verifyTypedData": "0x954c617864fd48aab85cea35987e43269041d641",
+  "masterTaxHandler": "0xd67c539c24b35da49c87655064fdc9f89ed64b13",
+  "masterLaunchWhitelist": "0x18df68db5d41526034fe99cca0517dc12b4b29c4",
   "pancakeRouter": "0xD99D1c33F9fC3444f8101754aBC46c52416550D1",
   "pancakeFactory": "0x6725F303b657a9451d8BA641348b6761A6CC7a17",
   "teamFinanceLocker": "0xD7eaa812Ab150A8E9a82A5b4107A83BA1F228dfA"
